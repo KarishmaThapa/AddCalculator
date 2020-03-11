dfa06696126ba9ae47018894effba5781081b367
KarishmaThapa <58963403+KarishmaThapa@users.noreply.github.com>
2020-03-11 13:26:14 +0545

    Add project files.

---
+++ WindowsFormsAddCalculator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAddCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(Number1.Text);
+            int n2 = Convert.ToInt32(Number2.Text);
+
+            int sumnum = n1 + n2;
+
+            Summation.Text = sumnum.ToString();
+        }
+    }
+}
